22a4942382e0d78566e1d34be562331fe23c9bfa
Kobus Viljoen <kobusv@gmail.com>
2017-12-21 17:22:32 +0200

    Updated war logic

--- back-end/python/python-OO-challenge.py
+++ back-end/python/python-OO-challenge.py
@@ -69,15 +69,18 @@ class Player:
         print('{} played {}'.format(self.name, card))
         return card
 
-    def draw_war_cards(self):
+    def draw_war_cards(self, draw=3):
         count = len(self.hand.cards)
-        if count < 3:
+        if count < draw:
             return [self.hand.remove() for i in range(len(self.hand.cards))]
         else:
-            return [self.hand.remove() for i in range(3)]
+            return [self.hand.remove() for i in range(draw)]
 
     def has_cards(self):
         return len(self.hand.cards) > 0
+
+    def cards_left(self):
+        return len(self.hand.cards)
 
 
 ######################
@@ -96,9 +99,52 @@ rounds = 0
 war_count = 0
 
 while(human.has_cards() and computer.has_cards()):
-    rounds++
+    rounds += 1
+    table_cards = []
 
+    h_card = human.play_card()
+    c_card = computer.play_card()
+    table_cards.extend([h_card, c_card])
 
+    if h_card[1] == c_card[1]:
+        print('War is on!')
+        war_count += 1
+        h_war_cards = human.draw_war_cards()
+        c_war_cards = computer.draw_war_cards()
+        table_cards.extend(h_war_cards)
+        table_cards.extend(c_war_cards)
+        war_still_on = True
 
+        while(war_still_on):
+            h_card = h_war_cards.pop()
+            c_card = c_war_cards.pop()
 
-# Use the 3 classes along with some logic to play a game of war!
+            if RANKS.index(h_card[1]) > RANKS.index(c_card[1]):
+                print('{} wins this round'.format(human.name))
+                human.hand.add(table_cards)
+                war_still_on = False
+            elif RANKS.index(h_card[1]) < RANKS.index(c_card[1]):
+                print('{} wins this round'.format(computer.name))
+                computer.hand.add(table_cards)
+                war_still_on = False
+            else:
+                war_count += 1
+                h_extra = human.draw_war_cards(2)
+                c_extra = computer.draw_war_cards(2)
+                table_cards.extend(h_extra)
+                h_war_cards.extend(h_extra)
+                table_cards.extend(c_extra)
+                c_war_cards.extend(c_extra)
+
+    else:
+        if RANKS.index(h_card[1]) > RANKS.index(c_card[1]):
+            print('{} wins this round'.format(human.name))
+            human.hand.add(table_cards)
+        else:
+            print('{} wins this round'.format(computer.name))
+            computer.hand.add(table_cards)
+
+print('Game over')
+print('a total of {} rounds was played'.format(rounds))
+print('War happened {} times'.format(war_count))
+print('The winner is {}'.format(human.name if human.cards_left() > computer.cards_left() else computer.name))
