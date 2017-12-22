ae16cc61f1e7cfc2c77fa4276e9073175d224c0a
Kobus Viljoen <kobusv@gmail.com>
2017-12-22 14:29:11 +0200

    added templates

--- back-end/django/first_project/first_app/views.py
+++ back-end/django/first_project/first_app/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello World!')
+    my_dict = {'insert_me': 'I just added new content here'}
+    return render(request, 'index.html', context=my_dict)
