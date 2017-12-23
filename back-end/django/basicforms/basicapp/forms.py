d67bfc52b951556c6eaf954f13c0c8ffd6a0f8da
Kobus Viljoen <kobusv@gmail.com>
2017-12-23 11:32:04 +0200

    added botcatcher

--- back-end/django/basicforms/basicapp/forms.py
+++ back-end/django/basicforms/basicapp/forms.py
@@ -1,6 +1,16 @@
 from django import forms
+from django.core import validators
 
 class MyForm(forms.Form):
-    name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Name'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control','placeholder':'Email'}))
-    text = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
+    name = forms.CharField(required=True,
+        widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Name'}))
+
+    email = forms.EmailField(required=True,
+        widget=forms.EmailInput(attrs={'class':'form-control','placeholder':'Email'}))
+
+    text = forms.CharField(required=False,
+        widget=forms.Textarea(attrs={'class': 'form-control'}))
+
+    botcatcher = forms.CharField(required=False,
+        widget=forms.HiddenInput,
+        validators=[validators.MaxLengthValidator(0)])
